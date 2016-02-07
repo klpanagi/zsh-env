d8c8539c65a9a8be8c1eadf7b82cca4c07db3430
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-02-07 17:51:07 +0200

    bullettrain customizations

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 currentDir=$(pwd)
 BulletTrainUrl="https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme"
+powerlevel9k_url="git@github.com:bhilburn/powerlevel9k.git"
 
 # Install zsh
 sudo apt-get install zsh
@@ -17,6 +18,9 @@ chsh -s $(which zsh)
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-cd "${HOME}/.oh-my-zsh/themes" && wget ${BulletTrainUrl}
+cd "${HOME}/.oh-my-zsh/custom/themes"
+wget ${BulletTrainUrl}
+git clone $powerlevel9k_url
+
 rm "${HOME}/.zshrc"
 cp "${currentDir}/my_zshrc" ~/.zshrc
